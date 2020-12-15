e71718fbd2809a8a2a88d1fa136faca48c609272
fixprogram <fixprogram@mail.ru>
2020-12-15 21:21:15 +0300

    Amazon clone set up commit and also was added a header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAZBdmYeTmsYj6juXXLb3dfu_OODc6LxxQ",
+  authDomain: "clone-5827f.firebaseapp.com",
+  projectId: "clone-5827f",
+  storageBucket: "clone-5827f.appspot.com",
+  messagingSenderId: "7259078706",
+  appId: "1:7259078706:web:9d3a61f20597a2d7b43c2b",
+  measurementId: "G-JNNFW55NF1",
+};
